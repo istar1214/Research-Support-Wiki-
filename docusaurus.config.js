5be69f8eacef66e73608912b6af6feb4fdbdcde2
istar1214 <istar86i@hanyang.ac.kr>
2026-07-09 10:39:07 +0900

    연구지원팀 WIKI 최신 수정본 백업

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
   organizationName: 'istar1214', // Usually your GitHub org/user name.
   projectName: 'Research-Support-Wiki-', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
